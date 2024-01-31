8de915cd622ed3248a2d96e24cb575487e3b8d6d
david.munoz.7e7 <david.munoz.7e7@itb.cat>
2024-01-31 11:26:13 +0100

    ex

--- e2.py
+++ e2.py
@@ -7,8 +7,8 @@ Yeray Lopez
 Mitjana de nombre aleatoris
 '''
 import random
-
-numeros = [random.randint(1, 50) for _ in range(100)]
+MAX= 100
+numeros = [random.randint(1, 50) for _ in range(MAX)]
 numeros_parelles = numeros[1::2]
 media_parelles = (sum(numeros_parelles) // len(numeros_parelles))
 numeros_senars = numeros[::2]
